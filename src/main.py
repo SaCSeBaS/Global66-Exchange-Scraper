3e5112f60cc4451003afd420553b66f511b883d5
SaCSeBaS <sebardoto@gmail.com>
2026-01-23 17:27:15 +0100

    Modified approach with requests

--- src/main.py
+++ src/main.py
@@ -19,22 +19,22 @@ def main(context):
     out_currency = "EUR"
     source = "https://www.global66.com/"
 
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "accept-language": "en-US,en;q=0.7",
-        "cookie": "cf_clearance=VU89Fv0KV1vMdqcCw0kYGO3aTfdlZnAVuuNuyYANWsk-1768848950-1.2.1.1-pIUfGDFSLfWY_.5gmSAke_F_42wpSf80FoQsP_fiFl02cHsE2QodG9iSUYiRunl1Z6U72MWCZFmeZMHZuehu1VwBmxqlY4QddISjikKRxeSAGxsXJODV0QflfT.hwtcS8NSSFC.UQK3meswSXey4gczB8twloswnWuivbyBNp4Q59pSH.OoUIYpByvvwxuF._HiQVldzc_oJy4AZQCsm5wggSZ7AxBU3xfohkWKuLN8",
-        "sec-ch-ua": '"Not(A:Brand";v="8", "Chromium";v="144", "Brave";v="144"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "sec-gpc": "1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
-    }
+    # headers = {
+    #     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    #     "accept-encoding": "gzip, deflate, br, zstd",
+    #     "accept-language": "en-US,en;q=0.7",
+    #     "cookie": "cf_clearance=VU89Fv0KV1vMdqcCw0kYGO3aTfdlZnAVuuNuyYANWsk-1768848950-1.2.1.1-pIUfGDFSLfWY_.5gmSAke_F_42wpSf80FoQsP_fiFl02cHsE2QodG9iSUYiRunl1Z6U72MWCZFmeZMHZuehu1VwBmxqlY4QddISjikKRxeSAGxsXJODV0QflfT.hwtcS8NSSFC.UQK3meswSXey4gczB8twloswnWuivbyBNp4Q59pSH.OoUIYpByvvwxuF._HiQVldzc_oJy4AZQCsm5wggSZ7AxBU3xfohkWKuLN8",
+    #     "sec-ch-ua": '"Not(A:Brand";v="8", "Chromium";v="144", "Brave";v="144"',
+    #     "sec-ch-ua-mobile": "?0",
+    #     "sec-ch-ua-platform": '"Windows"',
+    #     "sec-fetch-dest": "document",
+    #     "sec-fetch-mode": "navigate",
+    #     "sec-fetch-site": "none",
+    #     "sec-fetch-user": "?1",
+    #     "sec-gpc": "1",
+    #     "upgrade-insecure-requests": "1",
+    #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    # }
 
     params = {
         "originRoute": "227",
